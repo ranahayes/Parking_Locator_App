e9ed87f1c1e70ec646dbc3b82930e252a4d28eda
ranahayes <ranamike2004@gmail.com>
2023-11-19 17:31:55 +0000

    Initialised a new activity_saved_locations_list.xml

---
+++ app/src/main/java/com/example/parking_locator_app/SavedLocationsListActivity.java
@@ -0,0 +1,14 @@
+package com.example.parking_locator_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavedLocationsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_locations_list);
+    }
+}
