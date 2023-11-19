2209d971c5a5d12e1d7656f3d9f48f80c8871fb4
ranahayes <ranamike2004@gmail.com>
2023-11-19 17:34:13 +0000

    Create a basic data model java class for saved locations

---
+++ app/src/test/java/SavedLocation.java
@@ -0,0 +1,16 @@
+public class SavedLocation {
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    // Constructor, getters, and setters
+    public SavedLocation(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getName() { return name; }
+    public double getLatitude() { return latitude; }
+    public double getLongitude() { return longitude; }
+}
