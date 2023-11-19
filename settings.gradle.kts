7c987201420eda2c2e997f7e10b4206095979c07
ranahayes <ranamike2004@gmail.com>
2023-11-19 16:08:49 +0000

    Created Parking_Locator_App project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Parking_Locator_App"
+include(":app")
+ 
