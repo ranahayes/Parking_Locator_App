6493b34a5679a30b0e34f2246ba7b9b81a2c91c9
ranahayes <ranamike2004@gmail.com>
2023-12-08 19:51:23 +0000

    Added dependencies to build.gradle for ROOM, Added LocationDao.java for DAO methods

---
+++ app/src/main/java/com/example/parking_locator_app/LocationDao.java
@@ -0,0 +1,19 @@
+package com.example.parking_locator_app;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface LocationDao {
+    @Insert
+    void insert(SavedLocation location);
+
+    @Query("SELECT * FROM saved_locations")
+    List<SavedLocation> getAllLocations();
+
+    // Additional DAO methods can be defined here
+}
+
