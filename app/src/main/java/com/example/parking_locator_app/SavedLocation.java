754933e31cfe26580e83c4c7b08c7865e05ee8d9
ranahayes <ranamike2004@gmail.com>
2023-12-08 19:47:19 +0000

    Modified xml layout files to include search bar for saved locations, also modified the SavedLocation.java to be a ROOM entity

--- app/src/main/java/com/example/parking_locator_app/SavedLocation.java
+++ app/src/main/java/com/example/parking_locator_app/SavedLocation.java
@@ -15,4 +15,31 @@ public class SavedLocation {
     public String getName() { return name; }
     public double getLatitude() { return latitude; }
     public double getLongitude() { return longitude; }
+
+
+    private int id; // Unique identifier for each location
+    private String address; // Optional, to store the address of the location
+    private long timestamp; // Optional, to store the time when the location was saved
+    private String notes; // Optional, to store any notes about the location
+
+    // Modified constructor to include the new attributes
+    public SavedLocation(int id, String name, double latitude, double longitude, String address, long timestamp, String notes) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.address = address;
+        this.timestamp = timestamp;
+        this.notes = notes;
+    }
+
+    // Getter and setter methods for the new attributes
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+    public long getTimestamp() { return timestamp; }
+    public void setTimestamp(long timestamp) { this.timestamp = timestamp; }
+    public String getNotes() { return notes; }
+    public void setNotes(String notes) { this.notes = notes; }
 }
