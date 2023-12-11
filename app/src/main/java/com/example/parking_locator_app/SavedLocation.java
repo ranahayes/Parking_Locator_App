f380fad23b3b3e2a7ee5b23321d77e9e943965bd
ranahayes <ranamike2004@gmail.com>
2023-12-11 23:55:50 +0000

    Added constructor for SavedLocation along with Getters and Setters

--- app/src/main/java/com/example/parking_locator_app/SavedLocation.java
+++ app/src/main/java/com/example/parking_locator_app/SavedLocation.java
@@ -1,28 +1,56 @@
+
 package com.example.parking_locator_app;
 
-public class SavedLocation {
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity(tableName = "saved_locations")
+public class SavedLocation implements Serializable {
+
+    public SavedLocation() {
+        // Default constructor
+    }
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "name")
     private String name;
+
+    @ColumnInfo(name = "latitude")
     private double latitude;
+
+    @ColumnInfo(name = "longitude")
     private double longitude;
 
-    // Constructor, getters, and setters
+    @ColumnInfo(name = "address")
+    private String address;
+
+    @ColumnInfo(name = "timestamp")
+    private long timestamp;
+
+    @ColumnInfo(name = "notes")
+    private String notes;
+
+    // Constructors, getters, and setters (same as before)
+    // ...
+
+    public String getName() { return name; }
+    public double getLatitude() { return latitude; }
+    public double getLongitude() { return longitude; }
+    // Constructor for Room to use
     public SavedLocation(String name, double latitude, double longitude) {
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
     }
 
-    public String getName() { return name; }
-    public double getLatitude() { return latitude; }
-    public double getLongitude() { return longitude; }
-
-
-    private int id; // Unique identifier for each location
-    private String address; // Optional, to store the address of the location
-    private long timestamp; // Optional, to store the time when the location was saved
-    private String notes; // Optional, to store any notes about the location
-
-    // Modified constructor to include the new attributes
+    // Constructor to be ignored by Room
+    @Ignore
     public SavedLocation(int id, String name, double latitude, double longitude, String address, long timestamp, String notes) {
         this.id = id;
         this.name = name;
@@ -33,7 +61,9 @@ public class SavedLocation {
         this.notes = notes;
     }
 
-    // Getter and setter methods for the new attributes
+
+    // Getter and setter methods
+    public void setName(String name) { this.name = name; }
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
     public String getAddress() { return address; }
@@ -42,4 +72,13 @@ public class SavedLocation {
     public void setTimestamp(long timestamp) { this.timestamp = timestamp; }
     public String getNotes() { return notes; }
     public void setNotes(String notes) { this.notes = notes; }
+
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
