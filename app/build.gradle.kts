8b7437743ced35ad50c4c53084fc8593a92dd257
ranahayes <ranamike2004@gmail.com>
2023-12-11 23:57:35 +0000

    Added Google PLay maps dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,12 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("androidx.room:room-runtime:2.4.3" )
     annotationProcessor ("androidx.room:room-compiler:2.4.3")
+    implementation ("com.google.android.gms:play-services-maps:17.0.1")
 
 }
